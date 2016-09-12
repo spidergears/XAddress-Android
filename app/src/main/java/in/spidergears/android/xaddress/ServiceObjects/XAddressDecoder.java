51d7440275482cfe0bf473d2d44062811a3fe2fa
spidergears <findspidergears@gmail.com>
2016-09-12 15:52:42 +0530

    Added database for countires and states

--- app/src/main/java/in/spidergears/android/xaddress/ServiceObjects/XAddressDecoder.java
+++ app/src/main/java/in/spidergears/android/xaddress/ServiceObjects/XAddressDecoder.java
@@ -1,14 +1,6 @@
 package in.spidergears.android.xaddress.ServiceObjects;
 
 import android.content.Context;
-import android.util.Log;
-
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-
-import au.com.bytecode.opencsv.CSVReader;
-import in.spidergears.android.xaddress.R;
 
 /**
  * Created by spidergears on 20/08/16.
--- app/src/main/java/in/spidergears/android/xaddress/XAddressApplication.java
+++ app/src/main/java/in/spidergears/android/xaddress/XAddressApplication.java
@@ -2,9 +2,14 @@ package in.spidergears.android.xaddress;
 
 import android.app.Application;
 import android.content.Context;
-import android.content.Intent;
-
-import org.greenrobot.greendao.database.Database;
+import android.database.sqlite.SQLiteDatabase;
+import android.util.Log;
+import com.facebook.stetho.Stetho;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 import in.spidergears.android.xaddress.GreenDAO.DaoMaster;
 import in.spidergears.android.xaddress.GreenDAO.DaoSession;
@@ -16,16 +21,20 @@ public class XAddressApplication extends Application {
     private static Context appContext;
 
     private DaoSession daoSession;
-    private final String databaseName = "XAddressDB";
+    private final String databaseName = "xaddressdb";
 
     @Override
     public void onCreate() {
         super.onCreate();
         appContext = getApplicationContext();
-
-        DaoMaster.DevOpenHelper helper = new DaoMaster.DevOpenHelper(this, databaseName);
-        Database XAddressDB = helper.getWritableDb();
-        daoSession = new DaoMaster(XAddressDB).newSession();
+        Stetho.initializeWithDefaults(this);
+        copyDatabaseFromAssets(databaseName);
+        //DaoMaster.DevOpenHelper helper = new DaoMaster.DevOpenHelper(this, databaseName);
+        //Database XAddressDB = helper.getWritableDb();
+        //daoSession = new DaoMaster(XAddressDB).newSession();
+        SQLiteDatabase xAddressDB = SQLiteDatabase.openDatabase(getDatabasePath(databaseName).getPath(), null, SQLiteDatabase.OPEN_READWRITE);
+        DaoMaster daoMaster = new DaoMaster(xAddressDB);
+        daoSession = daoMaster.newSession();
     }
 
     public static Context getAppContext(){
@@ -34,6 +43,35 @@ public class XAddressApplication extends Application {
 
     public DaoSession getDaoSession() {
         return daoSession;
+    }
+
+    private void copyDatabaseFromAssets(String databaseName){
+        String outFilePath = getDatabasePath(databaseName).getPath();
+        File outFile = new File(outFilePath);
+        if (outFile.exists())
+                return;
+        // Open your local db as the input stream
+        try {
+            InputStream inStream = getResources().openRawResource(R.raw.xaddressdb);
+            outFile.getParentFile().mkdirs();
+            outFile.createNewFile();
+            OutputStream outStream = new FileOutputStream(outFile);
+
+            byte[] buffer = new byte[1024];
+            int length;
+            while ((length = inStream.read(buffer)) > 0) {
+                outStream.write(buffer, 0, length);
+            }
+
+            outStream.flush();
+            outStream.close();
+            inStream.close();
+            Log.d("XAddressDB", "copyDatabaseFromAssets: Success" );
+        }
+        catch (IOException ioe){
+            Log.e("XAddressDB", "copyDatabaseFromAssets: Error"+ ioe );
+            ioe.printStackTrace();
+        }
     }
 
 }
